ebc703813bc70aa19676bec2738a89f1cc7df7e6
KTee1986 <kirgumby1@gmail.com>
2025-04-19 13:25:29 -0400

    fix sheet

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      process.env.GOOGLE_PRIVATE_KEY, // ✅ direct use, no .replace
       ["https://www.googleapis.com/auth/spreadsheets"]
     );
 
